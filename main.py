5ebb4a1bfb7c27f6a09274346f882143b8b0b03d
docpanama <supergrade@gmail.com>
2026-03-22 21:06:30 -0600

    Update main.py

--- main.py
+++ main.py
@@ -62,7 +62,7 @@ def generate_aar(data: AARRequest):
     # Metadata
     doc.add_paragraph(f"Learner ID: {learner_id}")
     doc.add_paragraph(f"Certification Level: {data.certification_level}")
-    doc.add_paragraph(f"CCT Experience Level: {data.experience}")
+    doc.add_paragraph(f"Experience Level: {data.experience}")
     doc.add_paragraph(f"Scenario ID: {data.scenario_id}")
     doc.add_paragraph(f"Scenario Title: {data.scenario_title}")
 
